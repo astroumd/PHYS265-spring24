857a2550c5f0d41631b58869797b9ec940d73747
Carter Hall <crhall@umd.edu>
2024-04-22 12:38:40 -0400

    Add lab 2 folder
    Adding lab 2 folder and three files

---
+++ lab2/lab2lastname.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 15:26:27 2022
+Modified on Fri Mar 11 15:26:27 2022
+@author: Your name
+
+Description
+------------
+"""
+
+# Part 1 - Histogram the data
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure(1,figsize=(6,6))
+fig.clf()
+axes = [fig.add_subplot(321),\
+        fig.add_subplot(322),\
+        fig.add_subplot(323),\
+        fig.add_subplot(324),\
+        fig.add_subplot(325),\
+        fig.add_subplot(326)]
+
+# You can use axes[0], axes[1], ....  axes[5] to make the six histograms.
+
+# Your code goes here
+
+
+#%%
+
+# Part 2 - Table of measurements
+
+# Your code goes here
+
+
+#%%
+
+# Part 3 - Snells law plot and fit
+
+fig = plt.figure(2,figsize=(6,6))
+fig.clf()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+# You can use ax1 and ax2 for the Snell's law plot and the chi squared plot.
+
+# Your code goes here
+
+
+# Part 4 - Chi squared plot
+
+# Your code goes here
+
+
+
+
+
+######################################################
